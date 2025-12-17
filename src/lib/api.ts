33165c53b427f18df7f5021931970ee07681b641
jayasurya07200 <jayasurya.s@mediafirewall.ai>
2025-12-17 18:00:09 +0530

    new uri

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://40.90.232.96:8082";
+const BASE_URL = "https://enterprise-mediafirewall-ai.millionvisions.ai/";
 const PERSONA_ID = "sunil_shetty";
 
 export type Expertise = "actor" | "businessman" | "fitness" | "life_coach";
